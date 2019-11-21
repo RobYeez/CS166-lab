74b1bbc9d82559d12557f5e54c760488880f72d9
Robert Yee <Ryee002@ucr.edu>
2019-11-20 18:20:54 -0800

    working on queries

--- lab07/queries.sql
+++ lab07/queries.sql
@@ -13,5 +13,33 @@ SELECT SUM(
 )
 AS sum_red;
 
+SELECT S.supplier_id, S.supplier_name
+FROM supplier S
+WHERE
+(SELECT SUM(NYC.on_hand)
+FROM part_nyc NYC
+WHERE S.supplier_id = NYC.supplier)
+>
+(SELECT SUM(SFO.on_hand)
+FROM part_sfo SFO
+WHERE S.supplier_id = SFO.supplier)
+ORDER BY S.supplier_id;
 
+SELECT DISTINCT S.supplier_id, S.supplier_name
+FROM supplier S, part_nyc NYC
+WHERE S.supplier_id = NYC.supplier AND NYC.part_number IN
+(SELECT NYC.part_number
+FROM supplier S, part_nyc NYC
+WHERE S.supplier_id = NYC.supplier
+EXCEPT
+SELECT SFO.part_number
+FROM supplier S, part_sfo SFO
+WHERE S.supplier_id = SFO.supplier)
+ORDER BY S.supplier_id;
 
+UPDATE part_nyc P
+SET P.on_hand = P.on_hand - 10
+WHERE P.on_hand >= 10;
+
+DELETE FROM part_nyc P
+WHERE P.on_hand < 30;
