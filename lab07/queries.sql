60e01912340cb86bf1f39591feb2f62aa91a9ffa
Robert Yee <Ryee002@ucr.edu>
2019-11-20 18:47:01 -0800

    adjusted queries

--- lab07/queries.sql
+++ lab07/queries.sql
@@ -10,8 +10,7 @@ SELECT SUM(
   (SELECT COUNT(S.on_hand)
   FROM part_sfo S, color C
   WHERE S.color = C.color_id AND C.color_name = 'Red')
-)
-
+);
 
 SELECT S.supplier_id, S.supplier_name
 FROM supplier S
@@ -28,18 +27,23 @@ ORDER BY S.supplier_id;
 SELECT DISTINCT S.supplier_id, S.supplier_name
 FROM supplier S, part_nyc P
 WHERE S.supplier_id = P.supplier AND P.part_number IN
-(SELECT P.part_number
-FROM supplier S, part_nyc P
-WHERE S.supplier_id = P.supplier
+(SELECT P1.part_number
+FROM supplier S, part_nyc P1
+WHERE S.supplier_id = P1.supplier
 EXCEPT
 SELECT SFO.part_number
 FROM supplier S, part_sfo SFO
 WHERE S.supplier_id = SFO.supplier)
 ORDER BY S.supplier_id;
 
-UPDATE part_nyc P
-SET P.on_hand = P.on_hand - 10
-WHERE P.on_hand >= 10;
+UPDATE part_nyc 
+SET on_hand = on_hand - 10
+WHERE on_hand >= 10;
 
-DELETE FROM part_nyc P
-WHERE P.on_hand < 30;
+DELETE FROM part_nyc 
+WHERE on_hand < 30;
+
+-- test index
+SELECT COUNT(*)
+FROM part_nyc_on_hand_index P
+WHERE P > 70;
