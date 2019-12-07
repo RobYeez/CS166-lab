9dfef244e99c0d5184fe522d11a28353f54d4b32
Robert Yee <Ryee002@ucr.edu>
2019-12-06 18:52:59 -0800

    Rob

--- Project Phase3-Implementation/Phase3/java/DBProject.java
+++ Project Phase3-Implementation/Phase3/java/DBProject.java
@@ -1053,7 +1053,7 @@ public class DBProject {
 // "SELECT B.price FROM Booking B WHERE B.bookingDate >= '" + date1 + "' AND B.bookingDate <= '" + date2 + "' ORDER BY B.price DESC LIMIT '" + num + "';";
 
       try {
-         String esqlQuery = "SELECT M.name FROM MaintenanceCompany M, Repair R WHERE R.mCompany = M.cmpID ORDER BY (SELECT COUNT(R1.mCompany) FROM Repair R1) DESC LIMIT '" + num + "';";
+         String esqlQuery = "SELECT M.name FROM MaintenanceCompany M, Repair R WHERE R.mCompany = M.cmpID ORDER BY (SELECT COUNT(R1.mCompany) FROM Repair R1 Maintenance Company M2 WHERE M2.cmpID = R1.mCompany) DESC LIMIT '" + num + "';";
          esql.executeQuery(esqlQuery);
       }
       catch(Exception e) {
